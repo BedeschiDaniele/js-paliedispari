15fa58487fd5c5b2912bbf3bbca907dde15fc395
Daniele <daniele.bedeschi94@gmail.com>
2020-11-18 17:49:03 +0100

    Commit finale

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,8 +1,21 @@
 //Scrivere una funzione per capire se la parola e' palindroma
-var paroladigitata = prompt("Inserire una parola: ");
 var bool;
-bool=palindroma(paroladigitata);
-console.log(bool);
+
+
+var element = document.getElementById("verifica");
+element.addEventListener('click',
+ function() {
+   var paroladigitata = document.getElementById("testo").value;
+   console.log(paroladigitata);
+   bool=palindroma(paroladigitata);
+   // console.log(bool);
+   if (bool == true) {
+     document.getElementById("risultato").value = "La parola e' palindroma";
+   }else {
+     document.getElementById("risultato").value = "La parola non e' palindroma";
+   }
+ }
+);
 
 //------------------------------------------------------------------------------
 function palindroma(parola) {
